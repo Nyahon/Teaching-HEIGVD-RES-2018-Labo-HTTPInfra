93822320bf2d327a36808965503f696b83b51317
nyahon <yohann.meyer@gmail.com>
2018-06-06 13:51:00 +0200

    added clarifications in step1.sh

--- step1.sh
+++ step1.sh
@@ -3,16 +3,20 @@
 ##useful, uncomment if you're new to this game
 #sudo usermod -aG docker ${USER}
 
+#set name for our docker container. Could be taken from user, not done yet.
 imgName="aphpache"
 
 
+#-------------------Functions declarations---------------------------
 init() {
         ##laterholder
 	cd . 
 }
 
 create_net(){
-
+        #remove previously created subnet
+        docker network rm priv_net 2>/dev/null
+        #(re)create it
         docker network create --subnet=172.19.0.0/16 priv_net
 
 }
@@ -20,21 +24,25 @@ build_server() {
 init
 echo "name: ";
 echo $imgName;
-#build docker image
+#build docker image with files in directory 'docker' 
 docker build -t $imgName docker
 
-#stop previous image running
+#stop previous image running with name $imgName
 docker stop $(docker ps | grep $imgName | cut -f1 -d ' ')
+#remove previous builded images with name $imgName
 docker rm   $(docker ps | grep $imgName | cut -f1 -d ' ')
 
-#run image
 
 }
 
 run_server(){
-	
-	# docker run -e TZ=Europe/Zurich -d --net priv_net -p 9090:80 --ip 172.18.0.41 $imgName
-	docker run -e TZ=Europe/Zurich -d -p 9090:80 $imgName
+     # -e    : set environment variables
+     # TZ    : define timezone
+     # -d    : run as deamon
+     # --net : connect network to previously created subnet
+     # -p    : map container port 80 to machine port 9090
+     # --ip  : set a fixed ip
+	 docker run -e TZ=Europe/Zurich -d --net priv_net -p 9090:80 --ip 172.19.0.41 $imgName
 }
 
 authors() { echo "=====authors=======";
@@ -50,6 +58,7 @@ helpf() { echo "Docker builder for first part of HTTP lab, RES course : ";
            echo "     -b : build server docker";
        }
 
+#--------------------------Main Loop---------------------------------
 
 while getopts ":arbh" o; do
     echo ${o};
@@ -60,11 +69,9 @@ while getopts ":arbh" o; do
         r)
 	    run_server
             ;;
-        d)
-            ;;
         b)
             #imgName=${OPTARG}
-	    create_net
+	        create_net
             build_server
             ;;
         h)
