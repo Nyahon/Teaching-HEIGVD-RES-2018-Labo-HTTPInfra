2e2523a1b2ecfa6d62d1f376a9930e410ec236ce
johannamelly <johannamelly@gmail.com>
2018-06-14 15:49:50 +0200

    sticky session fonctionnelle

--- test-reverse-proxy/templates/config-template.php
+++ test-reverse-proxy/templates/config-template.php
@@ -7,28 +7,31 @@
 
 ?>
 <VirtualHost *:80>
+	Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
         ServerName demo.res.ch
+        <Location /balancer-manager>
+                SetHandler balancer-manager
+                Order Deny,Allow
+                Deny from all
+                Allow from all
+        </Location>
+        ProxyPass /balancer-manager !
 	<Proxy balancer://myclusterrandom>
-    		BalancerMember 'http://<?php print "$DYNAMIC_APP1"?>'
-    		BalancerMember 'http://<?php print "$DYNAMIC_APP2"?>'
+    		BalancerMember 'http://<?php print "$DYNAMIC_APP1"?>' route=1
+    		BalancerMember 'http://<?php print "$DYNAMIC_APP2"?>' route=2
+		ProxySet stickysession=ROUTEID
 	</Proxy>
 	
         ProxyPass '/api/random/' 'balancer://myclusterrandom/'
-        ProxyPassReverse '/api/random/' 'balancer://myclusterrandom/'
+	ProxyPassReverse '/api/random/' 'balancer://myclusterrandom/'
 
 	<Proxy balancer://mycluster>
-                BalancerMember 'http://<?php print "$STATIC_APP1"?>'
-                BalancerMember 'http://<?php print "$STATIC_APP2"?>'
+                BalancerMember 'http://<?php print "$STATIC_APP1"?>' route=3
+                BalancerMember 'http://<?php print "$STATIC_APP2"?>' route=4
+		ProxySet stickysession=ROUTEID
         </Proxy>
 
         ProxyPass '/' 'balancer://mycluster/'
         ProxyPassReverse '/' 'balancer://mycluster/'
 
-	<Location "/balancer-manager">
- 		SetHandler balancer-manager
- 		Order Deny,Allow
- 		Deny from all
- 		Allow from all
-	</Location>
- 	ProxyPass /balancer-manager !
 </VirtualHost>
